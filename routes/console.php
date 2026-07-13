25ca95d60df6db0dd10c97d3af2724dee049d195
Ramonymous <saya.ramapamungkas@gmail.com>
2026-07-13 18:53:21 +0700

    fix(print): schedule orphaned Playwright temp file cleanup every 15min

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,17 @@
 <?php
 
+use App\Jobs\CleanupOldPdfFiles;
+use App\Services\PlaywrightPdfService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Jobs\CleanupOldPdfFiles;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Jalankan setiap hari pada tengah malam
 Schedule::job(new CleanupOldPdfFiles)->daily();
+
+Schedule::call(function () {
+    app(PlaywrightPdfService::class)->cleanTemporaryFiles(1);
+})->everyFifteenMinutes();
